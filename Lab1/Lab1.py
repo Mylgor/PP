b45d1e759dcf858253ab68e0bc080278ff2fb7cf
Mylgor <vd9559@gmail.com>
2015-12-07 23:41:03 +0300

    edit lab1

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,52 +1,53 @@
+# -*- coding: utf-8 -*-
 import threading
 import string
 
-numbOfSymbol = 500
-numbOfString = 100
+numb_of_symbol = 500
+numb_of_string = 100
 
-def PrintLetter(letter):
-    for _ in range(numbOfSymbol):
-        print(letter, end="")
+def print_letter(letter):
+    for _ in range(numb_of_symbol):
+        print(letter, end = "")
     print()
 
-def GetThread(_threads):
-    for index in range(len(_threads)):
-        if not(_threads[index].isAlive()):
+def get_thread(threads):
+    for index in range(len(threads)):
+        if not(threads[index].isAlive()):
             return index
     return -1
 
-def none(countFlows):
-    threads = [threading.Thread()] * countFlows
+def none(count_flows):
+    threads = [threading.Thread()] * count_flows
     for letter in string.ascii_lowercase:
-        for _ in range(numbOfString):
-            ind = GetThread(threads)
+        for _ in range(numb_of_string):
+            ind = get_thread(threads)
             while ind == -1:
-                ind = GetThread(threads)
-            t = threading.Thread(target=PrintLetter, args=[letter])
+                ind = get_thread(threads)
+            t = threading.Thread(target=print_letter, args=[letter])
             t.start()
             threads[ind] = t
 
-def look(countFlows):
+def look(count_flows):
     lock = threading.Lock()
-    threads = [threading.Thread()] * countFlows
+    threads = [threading.Thread()] * count_flows
     for letter in string.ascii_lowercase:
-        for _ in range(numbOfString):
-            ind = GetThread(threads)
+        for _ in range(numb_of_string):
+            ind = get_thread(threads)
             while ind == -1:
-                ind = GetThread(threads)
-            t = threading.Thread(target=PrintLetter, args=[letter])
+                ind = get_thread(threads)
+            t = threading.Thread(target=print_letter, args=[letter])
             t.start()
             threads[ind] = t
 
-def semaphore(countFlows):
+def semaphore(count_flows):
     semaphore = threading.BoundedSemaphore(1)
-    threads = [threading.Thread()] * countFlows
+    threads = [threading.Thread()] * count_flows
     for letter in string.ascii_lowercase:
-        for _ in range(numbOfString):
-            ind = GetThread(threads)
+        for _ in range(numb_of_string):
+            ind = get_thread(threads)
             while ind == -1:
-                ind = GetThread(threads)
-            t = threading.Thread(target=PrintLetter, args=[letter])
+                ind = get_thread(threads)
+            t = threading.Thread(target=print_letter, args=[letter])
             t.start()
             threads[ind] = t
 
@@ -55,29 +56,30 @@ def event(countFlows):
     threads = [threading.Thread()] * countFlows
     event[-1].set()
     for letter in string.ascii_lowercase:
-        for _ in range(numbOfString):
-            ind = GetThread(threads)
+        for _ in range(numb_of_string):
+            ind = get_thread(threads)
             while ind == -1:
-                ind = GetThread(threads)
+                ind = get_thread(threads)
             event[ind - 1].wait()
             event[ind - 1].clear()
-            t = threading.Thread(target=PrintLetter, args=[letter])
+            t = threading.Thread(target=print_letter, args=[letter])
             event[ind].set()
             t.start()
             threads[ind] = t
 
-try:
-    answer = int (input("Choose\n1 - none\n2 - look\n3 - semaphore\n4 - event\nAnswer: "))
-    countFlows = int(input("Numb of flows: "))
-except ValueError:
-    print("error")
-    exit(1)
+if __name__ == '__main__':
+    try:
+        answer = int(input("Choose\n1 - none\n2 - look\n3 - semaphore\n4 - event\nAnswer: "))
+        count_flows = int(input("Numb of flows: "))
+    except ValueError:
+        print("error")
+        exit(1)
 
-if answer == 1:
-    none(countFlows)
-elif answer == 2:
-    look(countFlows)
-elif answer == 3:
-    semaphore(countFlows)
-elif answer == 4:
-    event(countFlows)
+    if answer == 1:
+        none(count_flows)
+    elif answer == 2:
+        look(count_flows)
+    elif answer == 3:
+        semaphore(count_flows)
+    elif answer == 4:
+        event(count_flows)
